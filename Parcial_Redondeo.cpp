ad8a0f705999bb5f67f075ebfd808c370925ae57
Daniel D'Onofrio <119988563+daniel-donofrio@users.noreply.github.com>
2023-05-11 18:46:41 -0300

    Add files via upload

---
+++ Parcial_Redondeo.cpp
@@ -0,0 +1,12 @@
+//Usar la función correcta para redondearr el número 199.69 a su entero más cercano.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double numero = 199.49;
+    numero = round(numero);
+    cout << numero;
+    return 0;
+}
---
+++ Parcial_ValorCout.cpp
@@ -0,0 +1,9 @@
+// ¿Qué valor muestra en consola la siguiente expresión?
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << 7 + ((8-6) * 5 - 2) / 2 - (5 + 10 / 5) * 3 + 1 << endl;
+return 0;
+}
