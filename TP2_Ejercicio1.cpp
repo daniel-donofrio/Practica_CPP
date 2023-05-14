2a76d96f4dec911343345d97cfa19afcd5e0db3f
Daniel D'Onofrio <119988563+daniel-donofrio@users.noreply.github.com>
2023-05-13 23:57:15 -0300

    Add files via upload

---
+++ TP2_Ejercicio1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double numero;
+
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+    cout << "La raiz cuadrada de " << numero  << " es: " << sqrt(numero) << endl;
+    return 0;
+}
