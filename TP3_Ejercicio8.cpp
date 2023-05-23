9b14c4cc48be7de5740fcdb2107ae5fb8197aa53
Daniel D'Onofrio <119988563+daniel-donofrio@users.noreply.github.com>
2023-05-23 09:27:54 -0300

    Add files via upload

---
+++ TP3_Ejercicio8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dia, mes;
+    cout << "Ingrese el día: ";
+    cin >> dia;
+    cout << "Ingrese el mes: ";
+    cin >> mes;
+
+    if (dia <= 0 || dia > 31 || mes <=0 || mes > 12) {
+        cout << "Ingrese un una fecha válida."
+    }
+
+    if (mes == 2) {
+        
+    }
+
+
+
+}
