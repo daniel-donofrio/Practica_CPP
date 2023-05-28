4053b0c7949964a2d1d1bc2f630d0cdd05a71afd
Daniel D'Onofrio <119988563+daniel-donofrio@users.noreply.github.com>
2023-05-28 02:29:53 -0300

    Add files via upload

---
+++ TP4_Ej1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero = 1;
+    while (numero <= 10)
+    {
+        cout << numero << endl;
+        numero++;
+    }
+    return 0;
+}
